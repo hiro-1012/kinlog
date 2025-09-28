3ce7b8f7d3a0d62f3ec2bb4415cad309e4d68031
hiro-1012 <156037220+hiro-1012@users.noreply.github.com>
2025-09-28 16:15:11 +0900

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,10 +21,10 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = nil
+  config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
